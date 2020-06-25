745ec2514da3092798aac2b1c5a122af9e873253
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-06-25 10:57:06 -0700

    trash drop not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -188,16 +188,16 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log("activate", this);
+    //console.log("activate", this);
   },
   deactivate: function(event) {
-    console.log("deactivate", this);
+    //console.log("deactivate", this);
   },
   over: function(event) {
-    console.log("over", event.target);
+    //console.log("over", event.target);
   },
   out: function(event) {
-    console.log("out", event.target);
+    //console.log("out", event.target);
   },
   update: function(event) {
     var tempArr = [];
@@ -230,6 +230,22 @@ $(".card .list-group").sortable({
     tasks[arrName] = tempArr;
     saveTasks();
 
-    console.log(tempArr);
+    //console.log(tempArr);
+  }
+});
+
+//create functionality for trash (drag-drop)
+$("trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    console.log("drop");
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
   }
 });
